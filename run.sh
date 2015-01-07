666a72f6116017b652b60e320ac867d0d9f43c34
swvitaliy <sw.vitaliy@gmail.com>
2015-01-07 14:10:17 +0300

    change path to project

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -t -i -v /home/vit/Projects/golang:/opt/golang swvitaliy/golang /bin/bash
+docker run -t -i -v /opt/Projects/golang:/opt/golang swvitaliy/golang /bin/bash
 
